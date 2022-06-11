c48208f4180da5dc40aa3b421de9664b1ddbffdf
ajinnoh <azin0901@naver.com>
2022-06-11 19:14:43 +0900

    1차원 배열 최댓값, 최소값 구하기

---
+++ practice/src/practice/Test05.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test05 {
+	public static void main(String[] args) {
+    
+		Scanner sc = new Scanner(System.in);
+        
+		int num = sc.nextInt();
+		int[] arr = new int[num];
+        
+		for (int i = 0; i < num; i++) {
+			arr[i] = sc.nextInt();
+		}
+        
+		sc.close();
+		Arrays.sort(arr);
+		System.out.print(arr[0] + " " + arr[num - 1]);
+	}
+}
