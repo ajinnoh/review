105e2bcbfc162898547cf5aa01e2e9734d3421af
ajinnoh <azin0901@naver.com>
2022-06-11 18:12:04 +0900

    곱셈(반복문)

---
+++ practice/src/practice/Test02.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Test02 {
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		
+		sc.close();
+		
+		for(int i = 1; i<10; i++) {
+			System.out.println(number+" * "+i+" = "+(number*i));
+		}
+	}
+}
